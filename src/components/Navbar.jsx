2a147a3a49cdf7a56cc565a53d765d54bfd53f1b
Harshad <jalondharaharshad007@gmail.com>
2026-01-13 17:40:32 +0530

    *

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
       <header className="bg-pink-400 text-black w-full h-[80px] flex justify-center">
         <div className="bg-amber-100 w-full max-w-7xl px-[100px]">
           <div className="pt-0 pb-0 px-[49px]">
-            <nav className="px-10">
+            <nav className="">
               Home
             </nav>
           </div>
